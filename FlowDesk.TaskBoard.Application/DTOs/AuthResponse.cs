ae800b6a830e7177866c57dea53eb06137f8fea5
GayashanDeshapriya <gayashan.deshapriya.dev@gmail.com>
2026-03-28 20:44:02 +0530

    Add authentication DTOs: AuthResponse, LoginRequest, RegisterRequest

---
+++ FlowDesk.TaskBoard.Application/DTOs/AuthResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowDesk.TaskBoard.Application.DTOs
+{
+
+    public sealed record AuthResponse(
+        string AccessToken,
+        DateTimeOffset ExpiresAtUtc,
+        Guid UserId,
+        string Email,
+        string FullName,
+        string Role);
+}
