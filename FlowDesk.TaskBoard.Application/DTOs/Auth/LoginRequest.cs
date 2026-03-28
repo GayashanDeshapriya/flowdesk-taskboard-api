d3d8edd33772e62454cacf96e3bef3ba6745c8ea
GayashanDeshapriya <gayashan.deshapriya.dev@gmail.com>
2026-03-28 23:03:36 +0530

    Refactor DTOs into Auth/Project namespaces, add Project DTOs

--- FlowDesk.TaskBoard.Application/DTOs/Auth/LoginRequest.cs
+++ FlowDesk.TaskBoard.Application/DTOs/Auth/LoginRequest.cs
@@ -1,3 +1,3 @@
-﻿namespace FlowDesk.TaskBoard.Application.DTOs;
+﻿namespace FlowDesk.TaskBoard.Application.DTOs.Auth;
 
 public sealed record LoginRequest(string Email, string Password);
--- FlowDesk.TaskBoard.Application/DTOs/Auth/RegisterRequest.cs
+++ FlowDesk.TaskBoard.Application/DTOs/Auth/RegisterRequest.cs
@@ -1,3 +1,3 @@
-﻿namespace FlowDesk.TaskBoard.Application.DTOs;
+﻿namespace FlowDesk.TaskBoard.Application.DTOs.Auth;
 
 public sealed record RegisterRequest(string Email, string FullName, string Password);
